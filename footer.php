1dd22622fc42e1eaedb4105425c967121f16f744
Kelly Brintle <kbrintle@gmail.com>
2013-03-08 17:37:35 -0500

    home page progress

---
+++ footer.php
@@ -0,0 +1,124 @@
+<div class="footer-container">
+    <div class="footer-top"><div><span></span></div></div>
+    <div class="footer">
+        <div class="f-left">
+            <form action="http://lnews.dietdirect.com/q/f2zDAIZ0rAQmHpg8AlD5hhskgOY1KSE68t" method="post" id="newsletter-validate-detail">
+                <input type="hidden" name="crvs" value="52tMD0kMehdoIfnneYIfhfixsVabAK3Jr610Cs_JxX2ZeJLn3IEkSzelkyaxhdTH4zB2HVpT4YAaPwnr5quYa2aOvAksqRU1QmuLp2NXWq8U9tPYRoVOcTHGkE3i7vm-">
+                <input type="hidden" name="Customer Info.Origin" value="web signup">
+                <input type="hidden" name="Email Preferences.Weekly Sale Announcements - Text" value="Subscribed">
+                <input type="hidden" name="Email Preferences.Special Discounts &amp; New Products - Text" value="Subscribed">
+                <input type="hidden" name="Email Preferences.Living Lighter Newsletter - Text" value="Subscribed">
+                <div class="form-subscribe">
+                    <div class="form-subscribe-header">Exclusive Email Offers</div>
+                    <label for="newsletter">Register to receive new product alerts, newsletters and special discount prices.</label>
+                    <div class="v-fix"><input name="email" type="text" id="newsletter" value="Email Address" onclick="this.value=='Email Address'?this.value='':''" onblur="this.value==''?this.value='Email address':''" class="input-text required-entry validate-email"></div>
+                    <div class="v-fix"><input name="Customer Info.First Name" type="text" id="nameonletter" value="First Name (Optional)" onclick="this.value=='First Name (Optional)'?this.value='':''" onblur="this.value==''?this.value='First Name (Optional)':''" class="input-text required-entry validate-empty"></div>
+                    <div class="v-fix"><input name="Customer Info.Last Name" type="text" id="lastnameonletter" value="Last Name (Optional)" onclick="this.value=='Last Name (Optional)'?this.value='':''" onblur="this.value==''?this.value='Last Name (Optional)':''" class="input-text required-entry validate-empty"></div>
+                    <button type="submit" class="button" title="Subscribe"><span><span>Submit</span></span></button>
+                </div>
+                <div class="clear">&nbsp;</div>
+                <div class="form-subscribe-foot ">&nbsp;</div>
+            </form>
+            <script type="text/javascript">
+                //<![CDATA[
+                var newsletterSubscriberFormDetail = new VarienForm('newsletter-validate-detail');
+                //]]>
+            </script>
+            <!--
+            Task 28413
+            Update social media links for the correct store.
+            Mike Z (mzhang@lyonscg.com)
+            -->
+
+            <div class="clear"></div>
+            <table width="270" cellpadding="5" cellspacing="20" class="social">
+                <tbody><tr><td colspan="3" height="20">&nbsp;</td></tr>
+                <tr>
+                    <td><a target="_blank" href="http://www.facebook.com/dietdirect"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/facebook.png" alt=""></a></td>
+                    <td><a target="_blank" href="http://twitter.com/#!/dietdirect"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/twitter.png" alt=""></a></td>
+                    <td><a target="_blank" href="https://plus.google.com/102160689846187269466"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/google.png" alt=""></a></td>
+                </tr>
+                <tr><td colspan="3" height="20">&nbsp;</td></tr>
+                </tbody></table>
+        </div>
+
+        <div class="footer-links">
+            <div class="footer-links-set">
+                <h4>Brands</h4>
+                <ul>
+                    <li><a href="/wonderslim-weight-loss-products-82.html">WonderSlim</a></li>
+                    <li><a href="/bari-wise-health-products-123.html">BariWise</a></li>
+                    <li><a href="/protidiet-pro-amino-products-103.html">ProtiDiet</a></li>
+                    <li><a href="/bariatric-advantage-vitamins-116.html">Bariatric Advantage</a></li>
+                </ul>
+                <a href="/brands.html">View All Brands</a></div>
+            <div class="footer-links-set">
+                <h4>Categories</h4>
+                <ul>
+                    <li><a href="/protein-diet-bars-15.html">Protein Bars</a></li>
+                    <li><a href="/protein-diet-shakes-drinks-12.html">Protein Shakes</a></li>
+                    <li><a href="/wonderslim-weight-loss-kits-3.html">Diet Plan Packages</a></li>
+                    <li><a href="/diet-weight-loss-snacks-21.html">Snacks</a></li>
+                    <li><a href="/diet-supplements.html">Vitamins &amp; Supplements</a></li>
+                    <li><a href="/new-diet-products-154.html">*NEW*&nbsp; Products</a></li>
+                    <li><a href="/weekly-sale-discounts-175.html">On Sale</a></li>
+                </ul>
+                <a href="/categories.html">View All Categories</a></div>
+            <div class="footer-links-set">
+                <h4>Information</h4>
+                <ul>
+                    <li><a href="catalogrequest/">Request a Catalog</a></li>
+                    <li><a href="http://dietweigh.com" target="_blank">Blog</a></li>
+                    <li><a title="Refer-A-Friend" href="/refer" target="_self">Refer-A-Friend</a></li>
+                    <li><a href="/diet-recipes.html">Recipes</a></li>
+                    <li><a href="/weight-loss-tips.html">Weight Loss Tips</a></li>
+                    <li><a href="/coupon-codes-volume-discounts.aspx">Discounts &amp; Coupons</a></li>
+                    <li><a href="/coupon-codes-volume-discounts.aspx#FreeSample">Free Sample</a></li>
+                    <li><a href="/wonderslim-success-stories.aspx">Success Stories</a></li>
+                    <li><a href="/diet-direct-rewards-program.html">Rewards Program</a></li>
+                </ul>
+            </div>				<div class="footer-links-set">
+            <h4>Help</h4>
+            <ul>
+                <li><a href="/customer/account/">My Account</a></li>
+                <li><a href="/contacts/">Contact Us</a></li>
+                <li><a href="/about.aspx">About Us</a></li>
+                <li><a href="/affiliate.aspx">Affiliate Program</a></li>
+                <li><a href="/faq.aspx ">FAQ's</a></li>
+                <li><a href="/privacy.aspx">Privacy Policy</a></li>
+                <li><a href="/ship.aspx">Shipping Info</a></li>
+                <li><a href="/return-policy.html">Return Policy</a></li>
+                <li><a title="Site Map" href="http://www.dietdirect.com/catalog/seo_sitemap/category/">Site Map</a></li>
+            </ul>
+            <ul class="links">
+            </ul>				</div>
+
+        </div>
+        <div class="clear"></div>
+        <table width="980" cellpadding="5" cellspacing="20" class="secur">
+            <tbody><tr>
+                <td>
+                    <!--
+                    Task: 28411
+                    BC McAfee Seal
+                    Mike Z (mzhang@lyonscg.com)
+                    -->
+                    <!-- START SCANALERT CODE -->
+                    <a target="_blank" href="https://www.mcafeesecure.com/RatingVerify?ref=www.dietdirect.com">
+                        <img width="94" height="54" border="0" src="//images.scanalert.com/meter/www.dietdirect.com/13.gif" alt="McAfee SECURE sites help keep you safe from identity theft, credit card fraud, spyware, spam, viruses and online scams" oncontextmenu="alert('Copying Prohibited by Law - McAfee SECURE is a Trademark of McAfee, Inc.'); return false;"></a>
+                    <!-- END SCANALERT CODE -->
+                </td>
+                <td align="center" valign="middle"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/olympics.png" alt=""></td>
+                <td align="center" valign="middle"><a target="_blank" href="http://www.stellaservice.com/profile/1384"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/stella.png" alt=""></a></td>
+                <td align="center" valign="middle"><a target="_blank" href="https://www.bbb.org/online/consumer/cks.aspx?id=1050825135742"><img src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/bbb.png" alt=""></a></td>
+                <td align="center" valign="middle"><img style="cursor: pointer;" onclick="verifySeal();" src="http://www.dietdirect.com/skin/frontend/dietdirect/dietdirect/images/dietdirect/goddady.png" alt=""></td>
+            </tr>
+            </tbody></table>
+        <div class="copyright">
+            <p>
+            </p><p>The products and the claims made about specific products on or through this site have not been evaluated by DietDirect.com or the Food and Drug Administration and are not approved to diagnose, treat, cure or prevent disease. The information provided on this site is for informational purposes only and is not intended as a substitute for advice from your physician or other health care professional or any information contained on any product label or packaging. You should not use the information on this site for diagnosis or treatment of any health problem or for prescription of any medication or other treatment. You should consult with a healthcare professional before starting any diet, exercise or supplementation program, before taking any medication, or if you have or suspect you might have a health problem.</p>			            <p></p>
+            <p>
+                Results not typical. As individuals vary, so do results. | Copyright ® 2013 Diet Direct. All Rights Reserved            </p>
+        </div>
+    </div>
+</div>
